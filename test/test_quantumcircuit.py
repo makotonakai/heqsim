2a14bc90f8da9fd09fb5f6c7f2eceb25770998bd
MakotoNakai <macofandohr1812@gmail.com>
2021-11-06 13:29:51 +0900

    Start writing test for QuantumCircuit module

---
+++ test/test_quantumcircuit.py
@@ -0,0 +1,18 @@
+from disqs.logical.circuit import QuantumCircuit
+from disqs.logical.gate import QuantumGate
+import unittest
+
+
+class TestQuantumCircuit(unittest.TestCase):
+
+    def test_gate_list(self):
+
+        qc = QuantumCircuit(1)
+        qc.x(0)
+
+        expected_gate_name = "X"
+        expected_gate_index = 0
+
+        gate = qc.gate_list[0]
+        assert gate.name == expected_gate_name
+        assert gate.index == expected_gate_index
