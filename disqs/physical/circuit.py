60b46fa56f5c1294fef399e8702b00192f69b1a7
MakotoNakai <macofandohr1812@gmail.com>
2021-11-07 00:37:48 +0900

    Update measure function

--- disqs/physical/circuit.py
+++ disqs/physical/circuit.py
@@ -67,37 +67,41 @@ class PhysicalCircuit:
         Args:
             idx (int): the index of a qubit that users measure
         """
-        prob = [prob_amp**2 for prob_amp in self.state]
 
-        prob_dict = {}
-        for state in range(len(prob)):
-            prob_dict[format(state, 'b').zfill(self.qubit_num)] = prob[state]
+        # Measurement probability of the measured qubit
+        measure_prob = {"0": 0, "1": 0}
 
-        measure_prob = [0, 0]
-        for state in list(prob_dict.keys()):
-            if state[idx] == "0" and prob_dict[state] > 0:
-                measure_prob[0] += 1
-            elif state[idx] == "1" and prob_dict[state] > 0:
-                measure_prob[1] += 1
-        measure_prob = [num / sum(measure_prob) for num in measure_prob]
-        measure_result = np.random.choice(range(2), 1, p=measure_prob)[0]
-
-        # 状態ベクトルを更新
+        # Pairs of state & its probability amplitude
         state_dict = {}
-        for state in range(len(prob)):
-            index = format(state, 'b').zfill(self.qubit_num)
-            state_dict[index] = self.state[state]
+        qubit_num = int(np.log2(len(self.state)))
+        for num in range(len(self.state)):
+            comp_basis = bin(num)[2:].zfill(qubit_num)
+            state_dict[comp_basis] = self.state[num]
 
+        # Calculate measurement probability of the measured qubit
+        for key in list(state_dict.keys()):
+            if key[idx] == "0":
+                measure_prob["0"] += state_dict[key]**2
+            else:
+                measure_prob["1"] += state_dict[key]**2
+
+        # Perform measurement
+        measure_result = np.random.choice(2, 1, p=list(measure_prob.values()))[0]
+
+        # Pairs of each of the updated states & its probability amplitude
         new_state_dict = {}
-        for state in list(state_dict.keys()):
-            if state[idx] == str(measure_result):
-                state_list = list(state)
-                del state_list[idx]
-                new_state = "".join(state_list)
+        for num in range(len(self.state)):
+            comp_basis = bin(num)[2:].zfill(qubit_num)
+            if comp_basis[idx] == str(measure_result):
+                comp_basis_list = list(comp_basis)
+                del comp_basis_list[idx]
+                new_comp_basis = "".join(comp_basis_list)
+                new_state_dict[new_comp_basis] = self.state[num]
 
-                if 1 in state_dict.values():
-                    new_state_dict[new_state] = state_dict[state]
-                else:
-                    new_state_dict[new_state] = state_dict[state] * np.sqrt(2)
+        # Update each of the probability amplitudes
+        for key in list(new_state_dict.keys()):
+            prob_list = [new_state_dict[key]**2 for key in list(new_state_dict.keys())]
+            new_state_dict[key] *= np.sqrt(1 / sum(prob_list))
 
+        self.state = np.array(list(new_state_dict.values()))
         return measure_result
