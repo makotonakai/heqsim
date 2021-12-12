858a88f439d232d3b1c7fb12876e21ce5450168a
MakotoNakai <macofandohr1812@gmail.com>
2021-12-12 17:25:39 +0900

    Add Cphase gate

--- disqs/logical/circuit.py
+++ disqs/logical/circuit.py
@@ -60,6 +60,12 @@ class QuantumCircuit:
         self.gate_list.append(QuantumGate("CNOT", control_index, target_index))
         self.gate_list.append(QuantumGate("RZ", target_index, theta=theta / 2))
 
+    def cphase(self, control_index, target_index, theta):
+        self.gate_list.append(QuantumGate("CNOT", control_index, target_index))
+        self.gate_list.append(QuantumGate("PHASE", target_index, theta=theta / 2))
+        self.gate_list.append(QuantumGate("CNOT", control_index, target_index))
+        self.gate_list.append(QuantumGate("PHASE", target_index, theta=theta / 2))
+
     def measure(self, index):
         self.gate_list.append(QuantumGate("Measure", index))
 
